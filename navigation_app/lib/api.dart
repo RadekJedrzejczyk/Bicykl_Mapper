651540ae2627e22ab9c391bbb7ce27d34c285ae4
RadekJedrzejczyk <101389133+RadekJedrzejczyk@users.noreply.github.com>
2025-01-09 13:21:02 +0100

    Delete navigation_app directory

--- navigation_app/lib/api.dart
+++
@@ -1,7 +0,0 @@
-const String baseUrl = 'https://api.openrouteservice.org/v2/directions/';
-const String apiKey = '5b3ce3597851110001cf6248f55d7a31499e40848c6848d7de8fa624';
-
-/// Funkcja generująca URL dla wybranego profilu (typu roweru)
-Uri getRouteUrl(String profile, String startPoint, String endPoint) {
-  return Uri.parse('$baseUrl$profile?api_key=$apiKey&start=$startPoint&end=$endPoint');
-}
